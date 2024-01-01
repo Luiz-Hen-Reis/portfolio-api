84180795be95eb80ac821fb27f7174f8b4c1472c
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-01 11:13:52 -0300

    chore: provide dotenv config to application

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module PortfolioApiV2
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    Dotenv::Railtie.load
     config.api_only = true
   end
 end
